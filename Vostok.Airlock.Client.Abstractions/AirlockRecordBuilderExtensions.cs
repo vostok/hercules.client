edd7c948b7797bb2cce214f7f4556a89ae1281db
ar1k <alex.ananjin@gmail.com>
2018-06-07 15:34:18 +0500

    Added ctor and and cast capabilities for TagValue

--- Vostok.Airlock.Client.Abstractions/AirlockRecordBuilderExtensions.cs
+++ Vostok.Airlock.Client.Abstractions/AirlockRecordBuilderExtensions.cs
@@ -16,37 +16,37 @@ namespace Vostok.Airlock.Client.Abstractions
             switch (tagValue.Type)
             {
                 case TagValueType.Byte:
-                    return source.Add(tagKey, (byte) tagValue.Source);
+                    return source.Add(tagKey, (byte) tagValue);
                 case TagValueType.Short:
-                    return source.Add(tagKey, (short) tagValue.Source);
+                    return source.Add(tagKey, (short) tagValue);
                 case TagValueType.Int:
-                    return source.Add(tagKey, (int) tagValue.Source);
+                    return source.Add(tagKey, (int) tagValue);
                 case TagValueType.Long:
-                    return source.Add(tagKey, (long) tagValue.Source);
+                    return source.Add(tagKey, (long) tagValue);
                 case TagValueType.Bool:
-                    return source.Add(tagKey, (bool) tagValue.Source);
+                    return source.Add(tagKey, (bool) tagValue);
                 case TagValueType.Float:
-                    return source.Add(tagKey, (float) tagValue.Source);
+                    return source.Add(tagKey, (float) tagValue);
                 case TagValueType.Double:
-                    return source.Add(tagKey, (double) tagValue.Source);
+                    return source.Add(tagKey, (double) tagValue);
                 case TagValueType.String:
-                    return source.Add(tagKey, (string) tagValue.Source);
+                    return source.Add(tagKey, (string) tagValue);
                 case TagValueType.ByteArray:
-                    return source.Add(tagKey, (byte[]) tagValue.Source);
+                    return source.Add(tagKey, (byte[]) tagValue);
                 case TagValueType.ShortArray:
-                    return source.Add(tagKey, (short[]) tagValue.Source);
+                    return source.Add(tagKey, (short[]) tagValue);
                 case TagValueType.IntArray:
-                    return source.Add(tagKey, (int[]) tagValue.Source);
+                    return source.Add(tagKey, (int[]) tagValue);
                 case TagValueType.LongArray:
-                    return source.Add(tagKey, (long[]) tagValue.Source);
+                    return source.Add(tagKey, (long[]) tagValue);
                 case TagValueType.BoolArray:
-                    return source.Add(tagKey, (bool[]) tagValue.Source);
+                    return source.Add(tagKey, (bool[]) tagValue);
                 case TagValueType.FloatArray:
-                    return source.Add(tagKey, (float[]) tagValue.Source);
+                    return source.Add(tagKey, (float[]) tagValue);
                 case TagValueType.DoubleArray:
-                    return source.Add(tagKey, (double[]) tagValue.Source);
+                    return source.Add(tagKey, (double[]) tagValue);
                 case TagValueType.StringArray:
-                    return source.Add(tagKey, (string[]) tagValue.Source);
+                    return source.Add(tagKey, (string[]) tagValue);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(tagValue.Type));
             }
