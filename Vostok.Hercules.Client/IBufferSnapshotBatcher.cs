1b52e6b7d99caf80e08ab44f40c45ea7dd690730
epeshk <peshkov@skbkontur.ru>
2019-02-27 22:46:40 +0500

    Splitted to files.

--- Vostok.Hercules.Client/IBufferSnapshotBatcher.cs
+++ Vostok.Hercules.Client/IBufferSnapshotBatcher.cs
@@ -7,42 +7,4 @@ namespace Vostok.Hercules.Client
     {
         IEnumerable<ArraySegment<BufferSnapshot>> Batch(BufferSnapshot[] snapshots);
     }
-
-    internal class BufferSnapshotBatcher : IBufferSnapshotBatcher
-    {
-        private readonly int maximumBatchSize;
-
-        public BufferSnapshotBatcher(int maximumBatchSize) =>
-            this.maximumBatchSize = maximumBatchSize;
-
-        public IEnumerable<ArraySegment<BufferSnapshot>> Batch(BufferSnapshot[] snapshots)
-        {
-            Array.Sort(snapshots, (a, b) => b.State.Length.CompareTo(a.State.Length));
-
-            var offset = 0;
-            var batchSize = 0;
-            var i = 0;
-
-            for (; i < snapshots.Length; i++)
-            {
-                var recordsLength = snapshots[i].State.LengthOfRecords;
-
-                if (batchSize + recordsLength > maximumBatchSize - Buffer.InitialPosition)
-                {
-                    if (batchSize > 0)
-                        yield return CreateSegment();
-                    offset = i;
-                    batchSize = 0;
-                }
-
-                batchSize += recordsLength;
-            }
-
-            if (batchSize > 0)
-                yield return CreateSegment();
-
-            ArraySegment<BufferSnapshot> CreateSegment() =>
-                new ArraySegment<BufferSnapshot>(snapshots, offset, i - offset);
-        }
-    }
 }
