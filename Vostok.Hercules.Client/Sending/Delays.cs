f413ed43cfbad548925c2d485e7acf4293f072d3
epeshk <peshkov@skbkontur.ru>
2019-03-04 15:46:51 +0500

    Renamed var.

--- Vostok.Hercules.Client/Sending/Delays.cs
+++ Vostok.Hercules.Client/Sending/Delays.cs
@@ -8,8 +8,8 @@ namespace Vostok.Hercules.Client.Sending
     {
         public static TimeSpan ExponentialWithJitter(TimeSpan sendPeriodCap, TimeSpan sendPeriod, int attempt)
         {
-            var delayMs = Math.Min(sendPeriodCap.TotalMilliseconds, sendPeriod.TotalMilliseconds * Math.Pow(2, attempt));
-            var delay = TimeSpan.FromMilliseconds(delayMs).Divide(2);
+            var baseDelayMs = Math.Min(sendPeriodCap.TotalMilliseconds, sendPeriod.TotalMilliseconds * Math.Pow(2, attempt));
+            var delay = TimeSpan.FromMilliseconds(baseDelayMs).Divide(2);
             var jitter = delay.Multiply(ThreadSafeRandom.NextDouble());
             return delay + jitter;
         }
