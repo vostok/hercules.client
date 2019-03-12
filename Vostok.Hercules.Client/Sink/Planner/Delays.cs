49eff8f2880b6598a08f82dfc769c42dbe76041d
epeshk <peshkov@skbkontur.ru>
2019-03-12 13:03:39 +0500

    Simplify delay calculation.

--- Vostok.Hercules.Client/Sink/Planner/Delays.cs
+++ Vostok.Hercules.Client/Sink/Planner/Delays.cs
@@ -8,10 +8,18 @@ namespace Vostok.Hercules.Client.Sink.Planner
     {
         public static TimeSpan ExponentialWithJitter(TimeSpan sendPeriodCap, TimeSpan sendPeriod, int attempt)
         {
+            const double maxJitterFraction = 0.2;
+            
             var baseDelayMs = Math.Min(sendPeriodCap.TotalMilliseconds, sendPeriod.TotalMilliseconds * Math.Pow(2, attempt));
-            var delay = TimeSpan.FromMilliseconds(baseDelayMs).Divide(2);
-            var jitter = delay.Multiply(ThreadSafeRandom.NextDouble());
+            
+            var delay = TimeSpan.FromMilliseconds(baseDelayMs);
+            
+            var jitter = delay.Multiply(Random(-maxJitterFraction, maxJitterFraction));
+            
             return delay + jitter;
         }
+
+        private static double Random(double from, double to)
+            => from + (to - from) * ThreadSafeRandom.NextDouble();
     }
 }
