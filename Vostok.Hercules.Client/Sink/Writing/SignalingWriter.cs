560b7843e1fca80e16d6e8ba581a3f1d11c66fd0
iloktionov <iloktionov@skbkontur.ru>
2019-03-16 21:50:19 +0500

    + SignalingWriter

---
+++ Vostok.Hercules.Client/Sink/Writing/SignalingWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Vostok.Commons.Threading;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Hercules.Client.Sink.Buffers;
+using Vostok.Hercules.Client.Sink.Statistics;
+
+namespace Vostok.Hercules.Client.Sink.Writing
+{
+    internal class SignalingWriter : IRecordWriter
+    {
+        private readonly IRecordWriter baseWriter;
+        private readonly IStatisticsCollector statistics;
+        private readonly AsyncManualResetEvent signal;
+        private readonly long transitionSignalThreshold;
+        private readonly long constantSignalThreshold;
+
+        public SignalingWriter(
+            IRecordWriter baseWriter, 
+            IStatisticsCollector statistics, 
+            AsyncManualResetEvent signal, 
+            long sizeLimit,
+            double transitionSignalFraction,
+            double constantSignalFraction)
+        {
+            this.baseWriter = baseWriter;
+            this.statistics = statistics;
+            this.signal = signal;
+
+            transitionSignalThreshold = (long)(sizeLimit * transitionSignalFraction);
+            constantSignalThreshold = (long)(sizeLimit * constantSignalFraction);
+        }
+
+        public RecordWriteResult TryWrite(IBuffer buffer, Action<IHerculesEventBuilder> build, out int recordSize)
+        {
+            var storedSizeBefore = statistics.EstimateStoredSize();
+
+            var result = baseWriter.TryWrite(buffer, build, out recordSize);
+
+            var storedSizeAfter = statistics.EstimateStoredSize();
+
+            if (storedSizeAfter >= constantSignalThreshold || 
+                storedSizeBefore < transitionSignalThreshold && storedSizeAfter >= transitionSignalThreshold)
+            {
+                signal.Set();
+            }
+
+            return result;
+        }
+    }
+}
