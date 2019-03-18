51e3cbec1be6c2c027b766f06569df8801f1c009
iloktionov <iloktionov@skbkontur.ru>
2019-03-18 21:55:26 +0500

    Tests for management client DTOs handling

---
+++ Vostok.Hercules.Client.Tests/Management/ManagementDtoConversion_Tests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Abstractions.Models;
+using Vostok.Hercules.Client.Abstractions.Queries;
+using Vostok.Hercules.Client.Management;
+
+namespace Vostok.Hercules.Client.Tests.Management
+{
+    [TestFixture]
+    internal class ManagementDtoConversion_Tests
+    {
+        [Test]
+        public void Should_convert_stream_creation_query_to_dto_and_back_to_description([Values(StreamType.Base, StreamType.Derived)] StreamType type)
+        {
+            var query = new CreateStreamQuery("my-stream")
+            {
+                Type = type,
+                TTL = 2.Days(),
+                Partitions = 35,
+                ShardingKey = new []{ "key" },
+                Sources = new [] {"foo", "bar"}
+            };
+
+            var dto = StreamDescriptionDtoConverter.CreateFromQuery(query);
+
+            var description = StreamDescriptionDtoConverter.ConvertToDescription(dto);
+
+            description.Should().BeEquivalentTo(query);
+        }
+
+        [Test]
+        public void Should_convert_timeline_creation_query_to_dto_and_back_to_description()
+        {
+            var query = new CreateTimelineQuery("my-timeline", new []{ "foo", "bar" })
+            {
+                Slices = 35,
+                TTL = 2.Days(),
+                TimetrapSize = 2.Seconds(),
+                ShardingKey = new[] { "key" },
+            };
+
+            var dto = TimelineDescriptionDtoConverter.CreateFromQuery(query);
+
+            var description = TimelineDescriptionDtoConverter.ConvertToDescription(dto);
+
+            description.Should().BeEquivalentTo(query);
+        }
+    }
+}
---
+++ Vostok.Hercules.Client.Tests/Management/ManagementDtoSerialization_Tests.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Management;
+using Vostok.Hercules.Client.Serialization.Json;
+
+namespace Vostok.Hercules.Client.Tests.Management
+{
+    [TestFixture]
+    internal class ManagementDtoSerialization_Tests
+    {
+        private readonly JsonSerializer serializer = new JsonSerializer();
+
+        [Test]
+        public void Should_serialize_empty_stream_description()
+        {
+            TestSerialization(new StreamDescriptionDto());
+        }
+
+        [Test]
+        public void Should_serialize_empty_timeline_description()
+        {
+            TestSerialization(new TimelineDescriptionDto());
+        }
+
+        [Test]
+        public void Should_serialize_filled_stream_description()
+        {
+            var description = new StreamDescriptionDto
+            {
+                Name = "my-stream",
+                Partitions = 10,
+                Sources = new[] { "foo", "bar" },
+                ShardingKey = new[] { "baz" },
+                TtlMilliseconds = 453534,
+                Type = "derived"
+            };
+
+            TestSerialization(description);
+        }
+
+        [Test]
+        public void Should_serialize_filled_timeline_description()
+        {
+            var description = new TimelineDescriptionDto
+            {
+                Name = "my-timeline",
+                Slices = 10,
+                Streams = new[] { "foo", "bar" },
+                ShardingKey = new[] { "baz" },
+                TtlMilliseconds = 453534,
+                TimetrapSizeMilliseconds = 5433
+            };
+
+            TestSerialization(description);
+        }
+
+        private void TestSerialization<T>(T item)
+        {
+            var serialized = serializer.Serialize(item);
+
+            var deserialized = serializer.Deserialize<T>(new MemoryStream(serialized));
+
+            deserialized.Should().BeEquivalentTo(item);
+        }
+    }
+}
