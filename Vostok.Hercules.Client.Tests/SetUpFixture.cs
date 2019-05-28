259c32549c38d5a0f6090e762d5b9d66fc2f403a
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-28 13:59:20 +0500

    set up thread pool in tests

---
+++ Vostok.Hercules.Client.Tests/SetUpFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Vostok.Commons.Threading;
+
+namespace Vostok.Hercules.Client.Tests
+{
+    [SetUpFixture]
+    internal class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            ThreadPoolUtility.Setup();
+        }
+    }
+}
