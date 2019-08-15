fece1bea815021eeea60698e2dc916c9e68d3003
kungurtsev <kungurtsev@skbkontur.ru>
2019-08-15 15:27:50 +0500

    test MemoryAnalyzer

---
+++ Vostok.Hercules.Client.Tests/Sink/Analyzer/MemoryAnalyzer_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Sink.Analyzer;
+
+namespace Vostok.Hercules.Client.Tests.Sink.Analyzer
+{
+    [TestFixture]
+    internal class MemoryAnalyzer_Tests
+    {
+        [Test]
+        public void ShouldFreeMemory_should_return_true_if_period_elapsed()
+        {
+            var analyzer = new MemoryAnalyzer(1.Minutes());
+            analyzer.ShouldFreeMemory(DateTime.UtcNow.Ticks - 50.Seconds().Ticks).Should().BeFalse();
+            analyzer.ShouldFreeMemory(DateTime.UtcNow.Ticks - 70.Seconds().Ticks).Should().BeTrue();
+        }
+    }
+}
