d8fd159e9a2b5cb70315289fe1d29d5151d434e0
kungurtsev <kungurtsev@skbkontur.ru>
2020-02-10 19:47:59 +0500

    use buffer pool

--- Vostok.Hercules.Client/Gate/GateRequestSender.cs
+++ Vostok.Hercules.Client/Gate/GateRequestSender.cs
@@ -6,6 +6,7 @@ using Kontur.Lz4;
 using Vostok.Clusterclient.Core;
 using Vostok.Clusterclient.Core.Model;
 using Vostok.Clusterclient.Core.Topology;
+using Vostok.Commons.Collections;
 using Vostok.Hercules.Client.Client;
 using Vostok.Logging.Abstractions;
 
@@ -13,70 +14,67 @@ namespace Vostok.Hercules.Client.Gate
 {
     internal class GateRequestSender : IGateRequestSender
     {
+        private const int MaxPooledBufferSize = 16 * 1024 * 1024;
+        private const int MaxPooledBuffersPerBucket = 8;
+
         private readonly IClusterClient client;
+        private readonly BufferPool bufferPool;
 
         public GateRequestSender(
             [NotNull] IClusterProvider clusterProvider,
             [NotNull] ILog log,
             [CanBeNull] ClusterClientSetup additionalSetup)
         {
-            ClusterClientSetup newAdditionalSetup = configuration =>
-            {
-                configuration.AddRequestTransform(request => Compress(request));
-                additionalSetup?.Invoke(configuration);
-            };
-
-            client = ClusterClientFactory.Create(clusterProvider, log, Constants.ServiceNames.Gate, newAdditionalSetup);
+            client = ClusterClientFactory.Create(clusterProvider, log, Constants.ServiceNames.Gate, additionalSetup);
+            bufferPool = new BufferPool(MaxPooledBufferSize, MaxPooledBuffersPerBucket);
         }
 
         public Task<Response> SendAsync(string stream, string apiKey, Content content, TimeSpan timeout, CancellationToken cancellationToken) =>
-            SendAsync("stream/send", stream, apiKey, r => r.WithContent(content), timeout, cancellationToken);
+            SendAsync("stream/send", stream, apiKey, content, timeout, cancellationToken);
 
         public Task<Response> FireAndForgetAsync(string stream, string apiKey, Content content, TimeSpan timeout, CancellationToken cancellationToken) =>
-            SendAsync("stream/sendAsync", stream, apiKey, r => r.WithContent(content), timeout, cancellationToken);
+            SendAsync("stream/sendAsync", stream, apiKey, content, timeout, cancellationToken);
 
         private async Task<Response> SendAsync(
             [NotNull] string path,
             [NotNull] string stream,
             [CanBeNull] string apiKey,
-            [NotNull] Func<Request, Request> addBody,
+            [NotNull] Content content,
             TimeSpan timeout,
             CancellationToken cancellationToken)
         {
             var request = Request.Post(path)
                 .WithAdditionalQueryParameter(Constants.QueryParameters.Stream, stream)
-                .WithContentTypeHeader(Constants.ContentTypes.OctetStream);
+                .WithContentTypeHeader(Constants.ContentTypes.OctetStream)
+                .WithContentEncodingHeader(Constants.Compression.Lz4Encoding)
+                .WithHeader(Constants.Compression.OriginalContentLengthHeaderName, content.Length);
 
             if (!string.IsNullOrEmpty(apiKey))
                 request = request.WithHeader(Constants.HeaderNames.ApiKey, apiKey);
 
-            request = addBody(request);
+            content = Compress(content);
 
-            var result = await client
-                .SendAsync(request, cancellationToken: cancellationToken, timeout: timeout)
-                .ConfigureAwait(false);
+            try
+            {
+                request = request.WithContent(content);
 
-            return result.Response;
-        }
+                var result = await client
+                    .SendAsync(request, cancellationToken: cancellationToken, timeout: timeout)
+                    .ConfigureAwait(false);
 
-        private Request Compress(Request request)
-        {
-            if (request.Content == null)
-                return request;
-
-            request = request
-                .WithContentEncodingHeader(Constants.Compression.Lz4Encoding)
-                .WithHeader(Constants.Compression.OriginalContentLengthHeaderName, request.Content.Length)
-                .WithContent(Compress(request.Content));
-
-            return request;
+                return result.Response;
+            }
+            finally
+            {
+                bufferPool.Return(content.Buffer);
+            }
         }
 
         private Content Compress(Content content)
         {
-            // TODO(kungurtsev): use buffer.
-            var newContent = LZ4Codec.Encode(content.Buffer, content.Offset, content.Length);
-            return new Content(newContent);
+            var buffer = bufferPool.Rent(content.Length + 1024);
+            var length = LZ4Codec.Encode(content.Buffer, content.Offset, content.Length, buffer, 0, buffer.Length);
+            return new Content(buffer, 0, length);
         }
     }
 }
