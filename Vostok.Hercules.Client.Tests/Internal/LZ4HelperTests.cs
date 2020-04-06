b724f518fe8e00e18749251e7c77aea7f1fe1937
kungurtsev <kungurtsev@skbkontur.ru>
2020-04-06 23:21:25 +0500

    lz4 tests

---
+++ Vostok.Hercules.Client.Tests/Internal/LZ4HelperTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Kontur.Lz4;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Internal;
+// ReSharper disable InconsistentNaming
+
+namespace Vostok.Hercules.Client.Tests.Internal
+{
+    [TestFixture]
+    public class LZ4HelperTests
+    {
+        [Test]
+        public void Should_be_enabled()
+        {
+            LZ4Helper.Enabled.Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_not_throw()
+        {
+            LZ4Codec.CompressBound(42);
+        }
+    }
+}
