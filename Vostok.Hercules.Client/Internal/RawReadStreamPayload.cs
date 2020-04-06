32ecc551132f5a75beda20c7f5bbb457408c3916
kungurtsev <kungurtsev@skbkontur.ru>
2020-04-06 20:03:51 +0500

    remove private assets

--- Vostok.Hercules.Client/Internal/RawReadStreamPayload.cs
+++ Vostok.Hercules.Client/Internal/RawReadStreamPayload.cs
@@ -7,14 +7,14 @@ namespace Vostok.Hercules.Client.Internal
     internal class RawReadStreamPayload : IDisposable
     {
         private readonly ValueDisposable<ArraySegment<byte>> content;
-        
+
         public RawReadStreamPayload(ValueDisposable<ArraySegment<byte>> content, StreamCoordinates next)
         {
             this.content = content;
             Content = content.Value;
             Next = next;
         }
-        
+
         public ArraySegment<byte> Content { get; }
 
         public StreamCoordinates Next { get; }
