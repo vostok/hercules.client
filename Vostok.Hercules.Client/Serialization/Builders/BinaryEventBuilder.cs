f527d4d7af5cbe44ad2ec86ea51460dfff4ad792
deniaa <deniaa@skbkontur.ru>
2021-06-24 19:11:24 +0500

    Improve performance of the Guid creating for Linux. There is a know issue that Guid.NewGuid() is 10 times slower on Linux.

--- Vostok.Hercules.Client/Serialization/Builders/BinaryEventBuilder.cs
+++ Vostok.Hercules.Client/Serialization/Builders/BinaryEventBuilder.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Vostok.Commons.Binary;
+using Vostok.Commons.Threading;
 using Vostok.Commons.Time;
 using Vostok.Hercules.Client.Abstractions.Events;
 
@@ -8,6 +11,10 @@ namespace Vostok.Hercules.Client.Serialization.Builders
 {
     internal class BinaryEventBuilder : IHerculesEventBuilder, IDisposable
     {
+        private static readonly Func<Guid> GenerateGuid = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? (Func<Guid>)GenerateGuidForLinux
+            : GuidNewGuid;
+
         private readonly IBinaryWriter binaryWriter;
         private readonly Func<DateTimeOffset> timeProvider;
 
@@ -25,7 +32,7 @@ namespace Vostok.Hercules.Client.Serialization.Builders
 
             timestampPosition = binaryWriter.Position;
             binaryWriter.Write(0L);
-            binaryWriter.Write(Guid.NewGuid());
+            binaryWriter.Write(GenerateGuid());
 
             tagsBuilder = new BinaryCountingTagsBuilder(binaryWriter);
         }
@@ -109,6 +116,28 @@ namespace Vostok.Hercules.Client.Serialization.Builders
 
             using (binaryWriter.JumpTo(timestampPosition))
                 binaryWriter.Write(EpochHelper.ToUnixTimeUtcTicks(timestamp.UtcDateTime));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe Guid GenerateGuidForLinux()
+        {
+            var bytes = stackalloc byte[16];
+            var dst = bytes;
+
+            var j = 0;
+            for (var i = 0; i < 4; i++)
+            {
+                *(int*)dst = ThreadSafeRandom.Next();
+                dst += 4;
+            }
+
+            return *(Guid*)bytes;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Guid GuidNewGuid()
+        {
+            return Guid.NewGuid();
         }
     }
 }
